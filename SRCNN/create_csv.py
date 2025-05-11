2bbe1ab6fa4d05c98849251223bac4c7703317e1
Peter Li <peterli@Peters-MacBook-Pro-2.local>
2025-05-11 13:28:14 -0500

    just before revising srcnn class to cater tiff

--- SRCNN/create_csv.py
+++ SRCNN/create_csv.py
@@ -4,10 +4,13 @@ import glob
 import pandas as pd
 import os
 
-### 
-#唯一需要修改的地方！！！
-###
-change='4_001' # IMPORTANT: choose the change from hr to lr here, '10_0015' or '10_001' or '4_001'
+"""
+THE ONLY CELL BELOW: MODIFY VALUES TO CATER
+"""
+
+image_dataset_name = 'TIFF_TEST_SINGLE' # image dataset folder name
+image_format = 'tif' # 'tif' or 'fits'
+change = '4_001' #  '4_001' or '10_001' or '10_0015'
 
 
 
@@ -16,25 +19,18 @@ name_list_hr = []
 name_list_lr = []
 ind=0
 
-se_hr_dir = "/Users/peterli/Desktop/BS_thesis/nicmos_se_hr_"+change
-se_lr_dir = "/Users/peterli/Desktop/BS_thesis/nicmos_se_lr_"+change
+se_hr_dir = '../../Prep_Images/'+image_dataset_name+'_se_hr_'+change # input: selective HR and LR images from preparation step
+se_lr_dir = '../../Prep_Images/'+image_dataset_name+'_se_lr_'+change
 
-for filename in glob.glob(os.path.join(se_hr_dir, "*.fits")):
-    # filename = filename.split("/")[-1]
+for filename in glob.glob(os.path.join(se_hr_dir, '*.'+image_format)):
     filename_hr = filename
     filename_lr = os.path.join(se_lr_dir, 'lr_' + filename.split("/")[-1])
     name_list_hr.append(filename_hr)
     name_list_lr.append(filename_lr)
 
-# # 原始的代码
-# for filename in glob.glob('/home/julia/SRCNN/sr_data/hr_images/*.fits'):
-#     filename = filename.split("/")[-1]
-#     filename_hr = 'sr_data/hr_images/' + filename
-#     filename_lr = 'sr_data/lr_images_10_noise_0015/lr_' + filename
-#     name_list_hr.append(filename_hr)
-#     name_list_lr.append(filename_lr)
-
 
 astropy_images['HR'] = name_list_hr
 astropy_images['LR'] = name_list_lr
-astropy_images.to_csv('srcnn_prep_images_'+change+'.csv', index=False)
+
+os.makedirs('srcnn_prep_filename', exist_ok=True)
+astropy_images.to_csv('srcnn_prep_filename/'+image_dataset_name+'_'+change+'.csv', index=False)
