7d7e7a0f70e09e3cf92beab70b38ce6406cb47e4
Peter Li <peterli@Peters-MacBook-Pro-2.local>
2025-05-10 19:00:17 -0500

    Add _ for Data Preparation
    move utils

--- visualize_fits.py
+++ visualize_fits.py
@@ -2,7 +2,8 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
 # 打开 FITS 文件
-hdul = fits.open("/Users/peterli/Desktop/BS_thesis/hide/nicmos_hr/nicmos_H_095746+0220_sci_25.fits")
+hdul = fits.open("/Users/peterli/Desktop/BS_thesis/nicmos_se_rl_sssim_10_001/rl_se_nicmos_H_095759+0239_sci_25.fits")
+
 image_data = hdul[0].data
 hdul.close()
 
